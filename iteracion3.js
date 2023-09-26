d6364780535e4d79e9119530fdf698dff0db526a
Natalia-dot <nataliadurantegarcia@gmail.com>
2023-09-26 15:45:20 +0200

    Ejercicios 26092023 JAVASCRIPT

---
+++ iteracion3.js
@@ -0,0 +1,26 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let multiply = 10*5
+console.log(multiply);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un console.
+let divide = 10/2;
+console.log(divide);
+
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.
+let resto = 15%9;
+console.log(resto);
+
+//1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+//teniendo dos variables p = 10 y j = 5
+const p = 10;
+const j = 5;
+let o = j+p;
+console.log(o);
+
+
+//1.5 Usa el correcto operador de asignación que resultará en i = 50,
+//teniendo dos variables c = 10 y m = 5.
+const c = 10;
+const m = 5;
+let i = m*c;
+console.log(i);
