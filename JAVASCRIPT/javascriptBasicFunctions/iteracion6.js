bdaeece89501bcb37b8e30d815fb9c4530c66193
Natalia-dot <nataliadurantegarcia@gmail.com>
2023-09-28 23:58:48 +0200

    Ultimos cambios

---
+++ JAVASCRIPT/javascriptBasicFunctions/iteracion6.js
@@ -0,0 +1,63 @@
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+  function removeDuplicates(param) {
+
+}
+
+
+const alumno = {
+    name: "Luis",
+    apellido: "Lopez",
+    direccion: {
+      calle: "castellana",
+      numero: 45,
+    },
+  };
+  console.log(alumno)
+  
+  alumno.name = "Abel";
+
+  console.log(alumno)
+  
+/* function showPokemon(name) {
+    // si name es indefinida o falsa, la establece a 'Magikarp'
+    name = name || 'Magikarp';
+  }
+
+  console.log(showPokemon())
+  function showPokemon(name) {
+    if (name === undefined) { // si falta el parámetro
+      name = 'Magikarp';
+    }
+  
+    console.log(name);
+  }
+  
+  showPokemon(); // Magikarp
+
+  const saludo = (name = "persona" || undefined ) => {
+    console.log(`Hola, ${name}.`)
+  }
+  saludo (" ")
+  //todo ------------Si se pone un string con un espacio, como se lo quito?------------------------
+
+  function showCount(count) {
+    // si count es undefined o null, muestra "desconocido"
+    console.log(count ?? "hola");
+  }
+  
+  showCount(0); // 0
+  showCount(null); // desconocido
+  showCount(); // desconocido
+*/
