130e379449191948969713a84c51d048b2eb95fc
Natalia-dot <nataliadurantegarcia@gmail.com>
2023-11-17 23:43:28 +0100

    Last Zelda fixes

--- REACT/Ejercicios React/8.-React-router-DOM/v6/src/pages/Home/Home.jsx
+++ REACT/Ejercicios React/8.-React-router-DOM/v6/src/pages/Home/Home.jsx
@@ -2,6 +2,6 @@ import "./Home.css"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home"><h1>Welcome to my first react page.</h1><img src="https://res.cloudinary.com/drbssyzr7/image/upload/v1700258352/GameHut/zelda_unkeb8.jpg"></img></div>
   )
 }
