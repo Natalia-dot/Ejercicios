2d05b217572a4c1598f12f8647a0b29d26e10471
Natalia-dot <nataliadurantegarcia@gmail.com>
2023-11-22 00:23:10 +0100

    Context added

--- REACT/Ejercicios React/12.-Usuario/src/contexts/Auth.context.jsx
+++ REACT/Ejercicios React/12.-Usuario/src/contexts/Auth.context.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom"; //useNavigate no lo vamos a utilizar aqui
 //porque el contexto esta fuera del router,por lo que no puede acceder a las rutas hijas
 
-const AuthorizationToken = createContext();
+const AuthorizationContext = createContext();
 //ex 1.Creamos el contexto
 
 export const authContextProvider = ({ children }) => {
@@ -17,7 +17,7 @@ export const authContextProvider = ({ children }) => {
 
     //ex aqui vamos a crear otro useState para setear toda la informacion completa del usuario,
     //ex que aun para el registration no la vamos a utilizar
-    const [completeUserInfo, setCompleteUSerInfo] = useState({
+    const [completeUserInfo, setCompleteUserInfo] = useState({
         data: {
             user: {
                 userEmail: "",
@@ -27,4 +27,51 @@ export const authContextProvider = ({ children }) => {
         }
     })
 
+    //!todo tenemos que hacer el puente aqui pero me espero a manana
+
+
+    //ex este es el login en el que vamos a meter el user en el localstorage que nos han pasado como string, y luego
+    //ex lo parseamos a objeto de javascript para poder settearlo como estado y poder referenciarlo en los servicios
+    //ex de autenticacion
+    const login = (data) => {
+        localStorage.setItem("user", data);
+        setUser(JSON.parse(data)) //fix esto se puede hacer?
+    }
+
+    //ex en el logout vamos a quitar los datos asociados a la clave "user" (que hemos metido en el login),
+    //ex y a borrar los datos del estado. Es todo deshacerlo de la misma manera que lo hicimos.
+    const logout = () => {
+        localStorage.removeItem("user");
+        setUser(null);
+    }
+
+    //ex por ultimo vamos a hacer un useMemo para que en el contexto persistan todas las funciones y setteos de 
+    //ex estado que hemos hecho. puesto que useMemo solo memoiza el return de las funciones, tenemos que retornar
+    //ex las funciones que queramos mantener dentro de este useMEmo
+
+    const value = useMemo(()=>({
+        user,
+        setUser,
+        completeUserInfo,
+        setCompleteUserInfo,
+        login,
+        logout,
+        //!todo falta el bridge!
+
+    }),[user])
+    //ex esta "dependencia", significa que va a volver a memoizar cuando cambie user, que la tenemos mas arriba
+    //ex (el estado)
+
+    return <AuthorizationContext.Provider value={value}>{children}</AuthorizationContext.Provider>
+    /*ex aqui estamos senalando a lo que acabamos de crear (authorizationContext), y estamos senalando
+    //ex a su provider, para establecerle el valor value al return de useMemo, por eso hemos utilizado
+    //ex useMemo, para poder devolverlo y que cambie a su vez cada vez que cambia [user], gracias a las
+    //ex dependencias y a su similitud con un useEffect sintacticamente(en cuanto a su significado), y 
+    //ex gracias a estas funciones con las que hemos provisto al provider con useMemo, podremos recurrir
+    //ex a ellas en toda nuestra aplicacion(children)
+    
+    
+    //ex */
 }
+
+export const useAuth = () => useContext(AuthorizationContext)
--- REACT/Ejercicios React/12.-Usuario/src/main.jsx
+++ REACT/Ejercicios React/12.-Usuario/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom"
 import "./index.css";
 import { router } from "./routes/routes.jsx";
+import { authContextProvider } from "./contexts/Auth.context.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <authContextProvider>
     <RouterProvider router={router} />
+    </authContextProvider>
   </React.StrictMode>
 );
